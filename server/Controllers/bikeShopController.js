26d6a669bb4aea125bf38d751f97613841f423ed
Sonu kumar <soryasonu420@gmail.com>
2026-08-03 18:08:44 +0530

    fix case sensitive Important for vercel

--- server/Controllers/bikeShopController.js
+++ server/Controllers/bikeShopController.js
@@ -1,76 +1,47 @@
-import BikeShop from "../models/BikeShop.js";
-
+import BikeShop from "../Models/BikeShop.js";
 
 // Add New Bike Shop
 
 export const addBikeShop = async (req, res) => {
+  try {
+    const bikeShop = await BikeShop.create(req.body);
 
-    try {
+    res.status(201).json({
+      success: true,
 
-        const bikeShop = await BikeShop.create(req.body);
+      message: "Bike Shop Added Successfully",
 
+      data: bikeShop,
+    });
+  } catch (error) {
+    console.log("Bike Shop Error:", error);
 
-        res.status(201).json({
+    res.status(500).json({
+      success: false,
 
-            success: true,
-
-            message: "Bike Shop Added Successfully",
-
-            data: bikeShop
-
-        });
-
-
-    } catch (error) {
-
-        console.log("Bike Shop Error:", error);
-
-
-        res.status(500).json({
-
-            success: false,
-
-            message: "Server Error"
-
-        });
-
-    }
-
+      message: "Server Error",
+    });
+  }
 };
-
-
 
 // Get All Bike Shops
 
 export const getBikeShops = async (req, res) => {
+  try {
+    const shops = await BikeShop.find();
 
-    try {
+    res.status(200).json({
+      success: true,
 
-        const shops = await BikeShop.find();
+      data: shops,
+    });
+  } catch (error) {
+    console.log(error);
 
+    res.status(500).json({
+      success: false,
 
-        res.status(200).json({
-
-            success:true,
-
-            data:shops
-
-        });
-
-
-    } catch(error){
-
-        console.log(error);
-
-
-        res.status(500).json({
-
-            success:false,
-
-            message:"Server Error"
-
-        });
-
-    }
-
+      message: "Server Error",
+    });
+  }
 };
